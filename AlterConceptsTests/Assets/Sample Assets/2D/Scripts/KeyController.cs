ab825300d04e855a12e5c525271fdd37927869d2
Jayson <jaysonperkins51@gmail.com>
2015-07-04 15:30:34 -0400

    Created fade script and Key Controller script

---
+++ AlterConceptsTests/Assets/Sample Assets/2D/Scripts/KeyController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyController : MonoBehaviour
+{
+		Vector2 startingPos = new Vector2 ();
+		// Use this for initialization
+		void Start ()
+		{
+				startingPos = gameObject.rigidbody2D.position;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
---
+++ AlterConceptsTests/Assets/scripts/FadeAction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeAction : MonoBehaviour
+{
+
+		private bool lessOpaque = true;
+		private float opacity = 100f;
+	
+		// Use this for initialization
+		void Start ()
+		{
+				Color colorHold = gameObject.renderer.material.color;
+				gameObject.renderer.material.color = new Color (colorHold.r, colorHold.g, colorHold.b, opacity / Utils.OneHundredFloat);
+		}
+		// Update is called once per frame
+		void Update ()
+		{
+				if (opacity > Utils.AmountToFadeTotal && lessOpaque) {
+						opacity -= Utils.AmountToFadePerFrame;
+				} else {
+						opacity += Utils.AmountToFadePerFrame;
+						lessOpaque = isOneHundredPercentOpaque ();
+				}
+		
+				Color colorHold = gameObject.renderer.material.color;
+				gameObject.renderer.material.color = new Color (colorHold.r, colorHold.g, colorHold.b, opacity / Utils.OneHundredFloat);
+		}
+	
+		private bool isOneHundredPercentOpaque ()
+		{
+				return opacity == Utils.OneHundredFloat;
+		}
+}
